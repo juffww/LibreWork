0b75454823d7950858c9aaf0eef46c4204a4c3b3
juffww <baoho1503@gmail.com>
2026-04-12 15:25:09 +0700

    first commit

---
+++ src/main/java/com/librework/LibreWorkApplication.java
@@ -0,0 +1,13 @@
+package com.librework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/librework/LibreWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
