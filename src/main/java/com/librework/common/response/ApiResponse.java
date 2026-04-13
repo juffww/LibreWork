fbff0d52cde38ee33d7c30f77a5c69f079f5b9ba
juffww <baoho1503@gmail.com>
2026-04-13 21:20:20 +0700

    feat(common): Add common that all modules use shared, excep and response

---
+++ src/main/java/com/librework/common/response/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.librework.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(true, "OK", data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
